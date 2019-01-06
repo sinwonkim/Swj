f69951fde5625fdca637b86b87a1948ceebd0b9b
sinwonkim <zhfps114@gmail.com>
2019-01-07 08:57:50 +0900

    # 로그인 구현

--- Swg/src/user/UserDAO.java
+++ Swg/src/user/UserDAO.java
@@ -23,4 +23,23 @@ public class UserDAO {
 		}
 	}
 	
+	public int login(String userID,String userPassword) { 
+		String SQL = "SELECT userPassword FROM USER WHERE userID =?"; 
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword)) {
+					return 1; // 1 로그인 성공
+				}
+				else 
+					return 0; // 0 로그인 실패 
+			}
+			return -1; // 아이디가 없음
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -2;// 데이터베이스 오류
+	
 }
